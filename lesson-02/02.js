7ed365177dee53d9744cdfd318b2ab7ee662173e
DmitriyStorozhuk <gelendzhik94@yandex.ru>
2024-08-26 23:46:09 +0300

    HW2JS2.2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,16 +7,14 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 = Math.floor(Math.random * 7);
-let dice2 = Math.floor(Math.random * 7);
-let isWinningDouble = dice1 > 3 === dice2 > 3;
+let dice1 = Math.floor(Math.random() * 6);
+let dice2 = Math.floor(Math.random() * 6);
+let isWinningDouble = dice1 > 3 && dice2 > 3;
 
-/*
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
+console.log("Первый бросок: " + dice1);
+console.log("Второй бросок: " + dice2);
 if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
+  console.log("Выигрышный дубль!");
 } else {
-  console.log('Не выигрышный дубль.')
+  console.log("Не выигрышный дубль.");
 }
-*/
