ca057365e6ccdcfdd159e3cd1621c2ae878c4f2e
DmitriyStorozhuk <gelendzhik94@yandex.ru>
2024-09-01 14:44:04 +0300

    HW3.JS1.3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,8 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(num1, num2, num3) {
+  const numCalc = Math.max([num1, num2, num3]);
+  return numCalc;
+  
+}
+
