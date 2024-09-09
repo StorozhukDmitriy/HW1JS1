5e4e65dc56444467c3fe0e1d57b5cb47e0e93433
DmitriyStorozhuk <gelendzhik94@yandex.ru>
2024-09-09 23:18:55 +0300

    HW4JS4.1

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -16,9 +16,9 @@ includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 function includesElement(array, element) {
   for (i = 0; i < array.length; i++) {
     if (element === array[i]) {
-      alert(true);
+      return true;
     } else {
-      alert(false);
+      return false;
     }
   }
 }
