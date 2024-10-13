33fefedb3fc2491ae26c72ec08d12468ed9557e0
DmitriyStorozhuk <gelendzhik94@yandex.ru>
2024-10-13 20:47:21 +0300

    HW9JS9.2

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -23,5 +23,17 @@ const doubledNumbers = map(numbers, (element, index) => {
 
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
-
-const map = () => {}
+const numbers = [1, 2, 3, 4, 5];
+const result = [];
+const map = (array, callback) => {
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    const index = i;
+    callback(element, index);
+  }
+  return result
+};
+const callback = (element, index) => {
+  return result.push(element * 2);
+};
+console.log(map(numbers, callback));
